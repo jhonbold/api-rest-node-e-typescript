6008c2496498e1b2a0391f86441f7472e3061d3b
jhonbold <jhonatassantosdj@gmail.com>
2023-02-22 11:00:47 -0300

    feat: config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 const server = express();
 
+//metodos BD: Get (buscar registro), Post (adicionar registro), Put (Atualizar registro), Delete (apagar registro) 
 server.get('/',(_, res) => {
 
     return res.send ('Olá, Dev! ');
